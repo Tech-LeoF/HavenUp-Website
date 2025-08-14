10517c2b33bd8f5c3036a7fa2c30a0e2421fe608
JDHA15 <josue.hernandez2026@hilasal.superate.org.sv>
2025-08-13 22:09:25 -0600

    login realizado :dizzy:

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+ 
+import { SignIn } from "@clerk/nextjs";
+import Login from "./login";
+ 
+export default function start(){
+    return(
+        <main className="min-h-screen flex items-center justify-center bg-gray-50">
+            <Login/>
+        </main>
+    )
+};
