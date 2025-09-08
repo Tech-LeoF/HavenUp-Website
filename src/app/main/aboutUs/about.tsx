0f97cbb6a71ba1526d223259f95edc90325433a9
keyUvi <keyli.ramirez2026@hilasal.superate.org.sv>
2025-09-07 18:09:43 -0600

    cambio

--- src/app/main/aboutUs/about.tsx
+++ src/app/main/aboutUs/about.tsx
@@ -2,7 +2,7 @@
 
 export default function About() {
     return (
-    <div style={{ fontFamily: "'Poppins', sans-serif" }} className="pr-8 pl-8"><br /><br />ñ
+    <div style={{ fontFamily: "'Poppins', sans-serif" }} className="pr-8 pl-8"><br /><br />
     <div className="flex items-center w-full">
         <img
         src="/Imgs/familia.webp"
