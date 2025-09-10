cf06b69bf9fc8b189dfcdfdbe7217ae8a09dd067
LAPT-CSH-15 <cshtd.computacion@gmail.com>
2025-09-10 10:03:42 -0600

    cambios en plans :bug:

--- src/app/PremiumPlan/premium.tsx
+++ src/app/PremiumPlan/premium.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const PricingPlans = () => {
   return (
-    <div style={{ fontFamily: "'Poppins', sans-serif" }} className="bg-gray-100 min-h-screen py-12 px-4 sm:px-6 lg:px-20">
+    <div style={{ fontFamily: "'Poppins', sans-serif" }} className="bg-white min-h-screen py-12 px-4 sm:px-6 lg:px-20">
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="text-3xl font-bold bg-gradient-to-b from-[#629584] to-gray-800 bg-clip-text text-transparent transition-colors duration-300">Choose the right plan for you</h2>
         <p className="mt-2 text-gray-500">Simple, transparent, and modern. Sell your property faster with the right tools</p>
         
         <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6">
         
-          <div className="h-100 w-90 rounded-xl p-6 bg-white shadow-xl border border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300  flex flex-col justify-between">
+          <div className="h-100 w-90 rounded-xl p-6 bg-white shadow-xl  border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300  flex flex-col justify-between">
             <div>
               <h3 className="flex justify-start items center p-2 text-lg font-semibold bg-gradient-to-b from-[#629584] to-gray-800 bg-clip-text text-transparent transition-colors">Basic plan</h3>
               <p className="flex flex-col items-start text-3xl font-bold text-blue-900 mt-2">Free</p>
@@ -25,7 +25,7 @@ const PricingPlans = () => {
             </button>
           </div>
 
-          <div className="h-100 w-90 h-full rounded-xl p-6 bg-white shadow-xl border border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300 flex flex-col justify-between">
+          <div className=" w-90 h-full rounded-xl p-6 bg-white shadow-xl  border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300 flex flex-col justify-between">
             <div>
               <h3 className="flex justify-start items center p-2 text-lg font-semibold bg-gradient-to-b from-[#629584] to-gray-800 bg-clip-text text-transparent transition-colors">Premium plan</h3>
               <p className="flex justify-start items center text-3xl font-bold text-blue-900 mt-2 transition-colors duration-300 ">$3<span className="text-base font-normal">.99/month</span></p>
@@ -41,7 +41,7 @@ const PricingPlans = () => {
             </button>
           </div>
 
-          <div className="h-100 w-90 h-full rounded-xl p-6 bg-white shadow-xl border border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300 flex flex-col justify-between">
+          <div className=" w-90 h-full rounded-xl p-6 bg-white shadow-xl  border-transparent border-3 hover:border-[#629584] transform hover:scale-105 transition-all duration-300 flex flex-col justify-between">
             <div>
               <h3 className="flex justify-start items center p-2 text-lg font-semibold bg-gradient-to-b from-[#629584] to-gray-800 bg-clip-text text-transparent transition-colors">Premier plan</h3>
               <p className="flex justify-start items-start text-3xl font-bold text-blue-900 mt-2 ">$7<span className="text-base font-normal">.99/month</span></p>
